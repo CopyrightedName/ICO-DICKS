ceb7b490e222b37218d80d546500eabe84496d7e
Kaos-DEV <37271227+Kaos-DEV@users.noreply.github.com>
2018-05-18 17:53:14 +0200

    Movement Changes
    Bounce, Friction

--- ICO-DICKS/Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
+++ ICO-DICKS/Assets/Standard Assets/Characters/RollerBall/Scripts/Ball.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace UnityStandardAssets.Vehicles.Ball
 {
@@ -9,6 +10,7 @@ namespace UnityStandardAssets.Vehicles.Ball
         [SerializeField] private bool m_UseTorque = true; // Whether or not to use torque to move the ball.
         [SerializeField] private float m_MaxAngularVelocity = 25; // The maximum velocity the ball can rotate at.
         [SerializeField] private float m_JumpPower = 2; // The force added to the ball when it jumps.
+
 
         private const float k_GroundRayLength = 1f; // The length of the ray to check if the ball is grounded.
         private Rigidbody m_Rigidbody;
@@ -28,19 +30,19 @@ namespace UnityStandardAssets.Vehicles.Ball
             if (m_UseTorque)
             {
                 // ... add torque around the axis defined by the move direction.
-                m_Rigidbody.AddTorque(new Vector3(moveDirection.z, 0, -moveDirection.x)*m_MovePower);
+                m_Rigidbody.AddTorque(new Vector3(moveDirection.z, 0, -moveDirection.x) * m_MovePower);
             }
             else
             {
                 // Otherwise add force in the move direction.
-                m_Rigidbody.AddForce(moveDirection*m_MovePower);
+                m_Rigidbody.AddForce(moveDirection * m_MovePower);
             }
 
             // If on the ground and jump is pressed...
             if (Physics.Raycast(transform.position, -Vector3.up, k_GroundRayLength) && jump)
             {
                 // ... add force in upwards.
-                m_Rigidbody.AddForce(Vector3.up*m_JumpPower, ForceMode.Impulse);
+                m_Rigidbody.AddForce(Vector3.up * m_JumpPower, ForceMode.Impulse);
             }
         }
     }
